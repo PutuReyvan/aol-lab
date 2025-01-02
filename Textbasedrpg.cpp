62f2a75df3664c2bd37c9038b38ac095614fe342
rey1010101 <108565817+PutuReyvan@users.noreply.github.com>
2025-01-02 22:19:10 +0700

    new feature
    Added battle feature and player stats

--- Textbasedrpg.cpp
+++ Textbasedrpg.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <conio.h>
+#include <time.h>
 
 // Generalized struct for resources
 typedef struct {
@@ -11,25 +12,44 @@ typedef struct {
 
 typedef struct{
 	int hp;
-	int xp;
 	int def;
 	int atk;
-}Stats;
+	int xp;
+	int level;
+}Player;
+
+typedef struct{
+	char name[50];
+	int hp;
+	int def;
+	int atk;
+}Mob;
 
 // Function prototypes
 
-/*function buat bagian login -rey*/
+/*function buat bagian login -rey
+*/
 void loginMenu();
 void registerUser();
 int loginUser();
 
+/*function buat stats etc -rey
+*/
+void gainXP(Player* player, int xpGain);
+void levelUp(Player* player);
+void showStats(Player *player);
+void cheatCode(Player *player);
+
+/*function buat battle -rey*/
+int calcDamage(int atk, int def);
+void battle(Player *player);
+Mob getRandomMob();
+
 void mainMenu();
-void miningMenu();
-void fishingMenu();
-void woodMenu();
-void activity(Resource *resource);
-
-
+void miningMenu(Player *player);
+void fishingMenu(Player *player);
+void woodMenu(Player *player);
+void activity(Resource *resource, Player* player);
 
 Resource copper = {"Copper", 0};
 Resource coal = {"Coal", 0};
@@ -40,12 +60,25 @@ Resource cemara = {"Kayu Cemara", 0};
 Resource jati = {"Kayu Jati", 0};
 Resource oak = {"Kayu Oak", 0};
 
+Mob mobs[] = {
+    {"Goblin", 50, 15, 5},
+    {"Orc", 80, 20, 10},
+    {"Troll", 120, 25, 15},
+    {"Slime", 30, 5, 4},
+    {"Lizardman", 150, 35, 10},
+    {"Lycan", 60, 15, 40}
+};
+
+int mobcount = sizeof(mobs)/sizeof(mobs[0]);
+
 // Main function
 int main() {
     int choice, login;
     int run = 1;
     int isAuth = 0;
-
+	
+	Player player= {100, 5, 2, 0, 1};
+	
 	while(!isAuth){
 		loginMenu();
     	printf("Enter your choice: ");
@@ -77,10 +110,13 @@ int main() {
         scanf("%d", &choice); getchar();
 
         switch (choice) {
-            case 1: miningMenu(); break;
-            case 2: fishingMenu(); break;
-            case 3: woodMenu(); break;
-            case 4: run = 0; break; // Exit
+            case 1: miningMenu(&player); break;
+            case 2: fishingMenu(&player); break;
+            case 3: woodMenu(&player); break;
+            case 4: showStats(&player); break; // Exit
+            case 5: battle(&player); break; // Exit
+            case 6: run = 0; break; // Exit
+            case 666: cheatCode(&player); break;
             default:
                 printf("Invalid choice. Please try again.\n");
                 Sleep(2000);
@@ -99,7 +135,9 @@ void mainMenu() {
     puts("[1] Mining");
     puts("[2] Fishing");
     puts("[3] Wood Cutting");
-    puts("[4] Exit");
+    puts("[4] Stats");
+    puts("[5] Battle");
+    puts("[6] Exit");
     puts("====================================");
 }
 
@@ -165,8 +203,103 @@ int loginUser(){
     return 0; // Login failed
 }
 
+int calcDamage(int atk, int def){
+	int damage = atk - def/2;
+	return damage >= 0 ? damage : 1;
+}
+
+Mob getRandomMob(){
+	srand(time(NULL));
+	return mobs[rand() % mobcount];
+}
+
+void battle(Player *player){
+	Mob mob = getRandomMob();
+	char choice;
+	
+	system("cls");
+	printf("A wild %s appears! (HP: %d, ATK: %d, DEF: %d)\n", 
+        mob.name, mob.hp, mob.atk, mob.def);
+    printf("Do you wish to fight? (Y/N): ");
+    scanf(" %c", &choice); getchar();
+
+    if (choice == 'N' || choice == 'n') {
+        printf("You chose to avoid the fight. Returning to main menu...\n");
+        Sleep(2000);
+        return;
+    }
+	
+	while(player->hp > 0 && mob.hp > 0){
+		int pDamage = calcDamage(player->atk, mob.def);
+		mob.hp -= pDamage;
+		printf("You hit the %s for %d damage! (%s HP: %d)\n", 
+                mob.name, pDamage, mob.name, mob.hp > 0 ? mob.hp : 0);
+                
+        if(mob.hp <= 0){
+        	printf("Congratulations, you defeated a %s!\n", mob.name);
+        	printf("You gained 100 XP and 5 Gold...");
+        	player->xp += 100;
+        	Sleep(2000);
+        	break;
+		}
+        
+		int mDamage = calcDamage(mob.atk, player->def);
+		player->hp -= mDamage;
+		
+		printf("The %s hits you for %d damage! (Your HP: %d)\n", 
+                mob.name, mDamage, player->hp > 0 ? player->hp : 0);
+
+        if (player->hp <= 0) {
+            printf("You were defeated by the %s...\n", mob.name);
+            Sleep(2000);
+            return;
+        }
+	
+        printf("Press enter to continue...."); getchar();
+	}
+	
+}
+
+void gainXP(Player* player, int xpGain){
+	player->xp += xpGain;
+	
+	if(player->xp >= (player->level*150)){
+		levelUp(player);
+	}
+}
+
+void cheatCode(Player *player){
+	player->hp = 999999;
+	player->atk = 999999;
+	player->def = 999999;
+	printf("Successfully applied cheat code. Now you're a Demon...\n"); Sleep(2000);
+}
+
+void showStats(Player *player) {
+    system("cls");
+    printf("====== Player Stats ======\n");
+    printf("HP: %d\n", player->hp);
+    printf("Attack: %d\n", player->atk);
+    printf("Defense: %d\n", player->def);
+    printf("XP: %d/%d\n", player->xp, player->level*150);
+    printf("==========================\n");
+    printf("Press Enter to return to the menu...\n");
+    getchar();
+}
+
+void levelUp(Player* player){
+	player->level++;
+	player->hp += 10;
+	player->atk += 2;
+	player->def += 2;
+	
+	printf("You've levelled up!\n");
+	printf("Press enter to continue..."); getchar();
+	
+}
+
 // Mining menu
-void miningMenu() {
+void miningMenu(Player *player) {
     int choice;
 
     system("cls");
@@ -178,8 +311,8 @@ void miningMenu() {
     scanf("%d", &choice); getchar();
 
     switch (choice) {
-        case 1: activity(&copper); break;
-        case 2: activity(&coal); break;
+        case 1: activity(&copper, player); break;
+        case 2: activity(&coal, player); break;
         case 3: return;
         default:
             printf("Invalid choice. Returning to the main menu...\n");
@@ -188,7 +321,7 @@ void miningMenu() {
 }
 
 // Fishing menu
-void fishingMenu() {
+void fishingMenu(Player *player) {
     int choice;
 
     system("cls");
@@ -201,9 +334,9 @@ void fishingMenu() {
     scanf("%d", &choice); getchar();
 
     switch (choice) {
-        case 1: activity(&cod); break;
-        case 2: activity(&cakalang); break;
-        case 3: activity(&tuna); break;
+        case 1: activity(&cod, player); break;
+        case 2: activity(&cakalang, player); break;
+        case 3: activity(&tuna, player); break;
         case 4: return;
         default:
             printf("Invalid choice. Returning to the main menu...\n");
@@ -211,7 +344,7 @@ void fishingMenu() {
     }
 }
 
-void woodMenu(){
+void woodMenu(Player *player){
 	int choice;
 	
  	system("cls");
@@ -224,9 +357,9 @@ void woodMenu(){
 	scanf("%d", &choice); getchar();
 	
 	switch(choice){
-		case 1: activity(&oak); break;
-		case 2: activity(&cemara); break;
-		case 3: activity(&jati); break;
+		case 1: activity(&oak, player); break;
+		case 2: activity(&cemara, player); break;
+		case 3: activity(&jati, player); break;
 		case 4: return;
 	    default:
             printf("Invalid choice. Returning to the main menu...\n");
@@ -235,7 +368,7 @@ void woodMenu(){
 }
 
 // General activity function
-void activity(Resource *resource) {
+void activity(Resource *resource, Player* player) {
     system("cls");
     
     while (1) {
@@ -246,6 +379,8 @@ void activity(Resource *resource) {
         system("cls");
         resource->count++;
         printf("You got 1 %s, Total: %d\n", resource->name, resource->count);
+        
+        gainXP(player, 10);
         
         // Check for quit condition
         if (kbhit()) { 
